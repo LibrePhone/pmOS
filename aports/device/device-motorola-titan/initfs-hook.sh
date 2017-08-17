ed0e48b611cf0a711a1cf63522dcaa396215166f
Pablo Castellano <pablo@anche.no>
2017-08-17 03:29:17 +0200

    device-motorola-titan: set usb properties
    The value in /sys/devices/virtual/android_usb/android0/iSerial
    is used by wcnss-service to generate some values to initialize
    the wireless interface after.

--- aports/device/device-motorola-titan/initfs-hook.sh
+++ aports/device/device-motorola-titan/initfs-hook.sh
@@ -2,3 +2,8 @@
 
 # set framebuffer resolution
 cat /sys/class/graphics/fb0/modes > /sys/class/graphics/fb0/mode
+
+# set usb properties
+echo -n "Motorola"    > /sys/devices/virtual/android_usb/android0/iManufacturer
+echo -n "Moto G 2014" > /sys/devices/virtual/android_usb/android0/iProduct
+echo -n "ZX1D229ZG4"  > /sys/devices/virtual/android_usb/android0/iSerial
