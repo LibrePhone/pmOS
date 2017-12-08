88bea7534f242149296a3703ec80de94b3061326
drebrez <drebrez@gmail.com>
2017-12-08 16:35:07 +0100

    [hildon-ui] Rotate display and touchscreen coordinates

--- aports/main/postmarketos-ui-hildon/xinitrc_hildon.sh
+++ aports/main/postmarketos-ui-hildon/xinitrc_hildon.sh
@@ -11,6 +11,14 @@ done
 # Start dbus and export its environment variables
 eval "$(dbus-launch --sh-syntax --exit-with-session)"
 
+. /etc/deviceinfo
+if [ ! -z "$deviceinfo_dev_touchscreen" ]; then
+	# walk through all the attributes of the parent devices and reverse the
+	# output to find and extract the name attribute of the top parent device
+	touch_name=$(udevadm info -a -n "${deviceinfo_dev_touchscreen}" | tac | grep -m1 "ATTRS{name}" | cut -d '"' -f2)
+	xinput set-prop "$touch_name" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+fi
+
 # Start X11 with Hildon
 export LC_MESSAGES=en_US.UTF-8
 exec hildon-desktop
