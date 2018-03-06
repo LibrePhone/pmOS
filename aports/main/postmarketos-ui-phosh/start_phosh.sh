af37bedca0892255403b79db0825791f0e7a3e5b
Bart Ribbers <bribbers@disroot.org>
2018-03-06 17:15:50 +0100

    Add Phosh UI package

---
+++ aports/main/postmarketos-ui-phosh/start_phosh.sh
@@ -0,0 +1,16 @@
+if test -z "${XDG_RUNTIME_DIR}"; then
+	export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
+	if ! test -d "${XDG_RUNTIME_DIR}"; then
+		mkdir "${XDG_RUNTIME_DIR}"
+		chmod 0700 "${XDG_RUNTIME_DIR}"
+	fi
+
+	if [ $(tty) = "/dev/tty1" ]; then
+		udevadm trigger
+		udevadm settle
+
+		sleep 2
+
+		/usr/libexec/phosh 2>&1 | logger -t "$(whoami):phosh"
+	fi
+fi
